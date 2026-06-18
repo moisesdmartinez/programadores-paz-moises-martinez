8c55c17f2e7dfc44c8414fe5461f0df722a6a2b7
Your Name <you@example.com>
2026-06-17 23:07:20 -0500

    Actividad semana 6

--- semana-6/server.js
+++ semana-6/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+});
 
 app.post('/registro', (req, res) => {
 
